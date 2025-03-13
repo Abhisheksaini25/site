c87b53fd4b2dd1fe79ae06aca5c266da73371062
Abhishek Saini <163615914+Abhisheksaini25@users.noreply.github.com>
2025-03-13 18:11:56 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -28,5 +28,5 @@ def contact(request):
     #return HttpResponse("Hello contact")
 
 def services(request):
-    return render(request, 'services.html')
+    return render(request, 'index.html')
     #return HttpResponse("Hello services")
